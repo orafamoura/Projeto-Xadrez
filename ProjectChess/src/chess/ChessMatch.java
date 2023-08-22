59daae14f4cdc55863fa34a44e1524fb65874463
orafamoura <raphaelmvb2@gmail.com>
2023-08-22 17:09:28 -0300

    Implementing possible moves of Rook

--- ProjectChess/src/chess/ChessMatch.java
+++ ProjectChess/src/chess/ChessMatch.java
@@ -29,6 +29,7 @@ public class ChessMatch { //nessa classe vai ter as regras do xadrez
 		Position source = sourcePosition.toPosition(); // convertemos para posicao da matriz
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source); // validamos se tem uma peca nessa posicao
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target); 
 		return (ChessPiece)capturedPiece; //downcasting para chessPiece, pois a peca capturada era do tipo Piece, como vimos em cima.
 	}
@@ -49,6 +50,14 @@ public class ChessMatch { //nessa classe vai ter as regras do xadrez
 			throw new ChessException("there is no possible moves for the chosen piece");
 		}
 	}
+	
+	private void validateTargetPosition(Position source, Position target) {
+		if(!board.piece(source).possibleMove(target)) {
+			throw new ChessException("The chosen piece can't move to target position");
+		}
+	}
+	
+	
 	private void placeNewPiece(char column, int row, ChessPiece piece) { //  uma operacao de colocar pecas passando as posicoes na coordenadas  do xadrez
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
