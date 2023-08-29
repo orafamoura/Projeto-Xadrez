09715a441f869bac676e4d9d0ae5552e599b12dc
orafamoura <raphaelmvb2@gmail.com>
2023-08-29 20:53:20 -0300

    added Bishop and fixed the Pawn

--- ProjectChess/src/chess/pieces/Pawn.java
+++ ProjectChess/src/chess/pieces/Pawn.java
@@ -47,15 +47,15 @@ public class Pawn extends ChessPiece{
 				mat[p.getRow()][p.getColumn()] = true;
 			}
 			p.setValues(position.getRow() +2, position.getColumn());
-			Position p2 = new Position(position.getRow() -1, position.getColumn());
+			Position p2 = new Position(position.getRow() +1, position.getColumn());
 			if(getBoard().positionExists(p) && !getBoard().ThereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().ThereIsAPiece(p2) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;		
 			}
-			p.setValues(position.getRow() +1, position.getColumn() -1);
+			p.setValues(position.getRow() +1, position.getColumn() +1);
 			if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
-			p.setValues(position.getRow() +1, position.getColumn() +1);
+			p.setValues(position.getRow() +1, position.getColumn() -1);
 			if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
